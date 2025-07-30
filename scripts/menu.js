bd1eef6257f32049bd5697e38fb7f15ba7c68017
AyeshaRana <rana.ayesha590@gmail.com>
2025-07-30 13:02:26 +0500

    completed about us page

---
+++ scripts/menu.js
@@ -0,0 +1,8 @@
+    function openSideBar() {
+
+            document.querySelector(".menu").style.display = "block";
+        }
+        function closeSideBar() {
+            document.querySelector(".menu").style.display = "";
+
+        }
